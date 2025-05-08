b8779a4cd47966bf78490f78fe617c3461a0f40f
Matias Charriere <matias@giantswarm.io>
2025-05-08 07:52:52 +0200

    Add proxysocks_user_connect_total metric
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,21 +6,30 @@ import (
 	"log"
 	"os"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/things-go/go-socks5"
+)
+
+var (
+	userConnectMetric = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "proxysocks_user_connect_total",
+		Help: "The total number of user connections",
+	})
 )
 
 func New() *socks5.Server {
 	logger := log.New(os.Stdout, "socks5: ", log.LstdFlags)
-
-	// Get credentials from environment variables
-	username := os.Getenv("PROXY_USERNAME")
-	password := os.Getenv("PROXY_PASSWORD")
 
 	// Setup server options
 	opts := []socks5.Option{
 		socks5.WithLogger(socks5.NewLogger(logger)),
 		socks5.WithConnectMiddleware(UserConnect),
 	}
+
+	// Setup auth
+	username := os.Getenv("PROXY_USERNAME")
+	password := os.Getenv("PROXY_PASSWORD")
 
 	var authenticator socks5.Authenticator
 	if username != "" && password != "" {
@@ -36,11 +45,14 @@ func New() *socks5.Server {
 	}
 	opts = append(opts, socks5.WithAuthMethods([]socks5.Authenticator{authenticator}))
 
+	// Setup metrics
+
 	server := socks5.NewServer(opts...)
 	return server
 }
 
 func UserConnect(ctx context.Context, writer io.Writer, request *socks5.Request) error {
+	userConnectMetric.Inc()
 	log.Printf("socks5: new connection from/to: %s %s", request.RemoteAddr, request.DestAddr)
 	return nil
 }
